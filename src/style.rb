ccfad931f85732c9571c72b5ae8e904a1a53a898
Jan Stanik <js@creat.io>
2015-09-05 23:31:26 +0100

    SLIP-49 Attempt to fix distorted characters.

--- src/style.rb
+++ src/style.rb
@@ -19,7 +19,11 @@ module Io
     # decorates style with units
     public
     def self.svg_dec_style_units ( style = SVG_STYLE )
-      return style.merge( { :letter_spacing => "%gem" % style[:letter_spacing] } ) 
+      return style.merge(
+      {
+        :stroke_width => "%fmm" % style[:stroke_width]
+        :letter_spacing => "%gem" % style[:letter_spacing]
+      } ) 
     end # svg_dec_style_units
 
   end # Creat
