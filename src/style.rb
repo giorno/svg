2dc3f42125e8a3629552626f9e41be4edaf770d0
Jan Stanik <js@creat.io>
2015-09-06 20:31:51 +0100

    SLIP-49 Separate generation of line styles from text styles.

--- src/style.rb
+++ src/style.rb
@@ -16,14 +16,24 @@ module Io
       :text_anchor    => 'left',
     }
 
+    SVG_STYLE_LINE = 0;
+    SVG_STYLE_TEXT = 1;
+
     # decorates style with units
     public
-    def self.svg_dec_style_units ( style = SVG_STYLE )
-      return style.merge(
+    def self.svg_dec_style_units ( style = SVG_STYLE, purpose = SVG_STYLE_LINE )
+      raise "Only SVG_STYLE_LINE or SVG_STYLE_TEXT are supported" unless [ SVG_STYLE_LINE, SVG_STYLE_TEXT ].include?( purpose ) 
+      copy = style.merge(
       {
         :stroke_width => "%fmm" % style[:stroke_width],
         :letter_spacing => "%gem" % style[:letter_spacing],
-      } ) 
+      } )
+      if purpose == SVG_STYLE_TEXT:
+        copy.delete( :stroke_width )
+      else # SVG_STYLE_LINE
+        return copy.merge( { :fill => 'none' } )
+      end
+      return copy
     end # svg_dec_style_units
 
   end # Creat
